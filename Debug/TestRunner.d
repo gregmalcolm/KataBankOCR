6ee59c0425974afe568f126141256384db58f2b4
gregmalcolm <greg@gregmalcolm.com>
2012-09-08 23:25:28 -0400

    C++ starting kata template

---
+++ Debug/TestRunner.d
@@ -0,0 +1,103 @@
+TestRunner.d TestRunner.o: ../TestRunner.cpp \
+  /Users/greg/eclipse/KataBankOCR/Externals/cpputest/include/CppUTest/CommandLineTestRunner.h \
+  /Users/greg/eclipse/KataBankOCR/Externals/cpputest/include/CppUTest/TestHarness.h \
+  /Users/greg/eclipse/KataBankOCR/Externals/cpputest/include/CppUTest/Utest.h \
+  /Users/greg/eclipse/KataBankOCR/Externals/cpputest/include/CppUTest/SimpleString.h \
+  /Users/greg/eclipse/KataBankOCR/Externals/cpputest/include/CppUTest/StandardCLibrary.h \
+  /usr/include/c++/v1/string /usr/include/c++/v1/__config \
+  /usr/include/c++/v1/iosfwd /usr/include/c++/v1/cstring \
+  /usr/include/c++/v1/cstdio /usr/include/c++/v1/cwchar \
+  /usr/include/c++/v1/cwctype /usr/include/c++/v1/cctype \
+  /usr/include/c++/v1/algorithm /usr/include/c++/v1/initializer_list \
+  /usr/include/c++/v1/cstddef /usr/include/c++/v1/type_traits \
+  /usr/include/c++/v1/utility /usr/include/c++/v1/__tuple \
+  /usr/include/c++/v1/memory /usr/include/c++/v1/typeinfo \
+  /usr/include/c++/v1/exception /usr/include/c++/v1/cstdint \
+  /usr/include/c++/v1/new /usr/include/c++/v1/limits \
+  /usr/include/c++/v1/iterator /usr/include/c++/v1/__functional_base \
+  /usr/include/c++/v1/cstdlib /usr/include/c++/v1/stdexcept \
+  /Users/greg/eclipse/KataBankOCR/Externals/cpputest/include/CppUTest/UtestMacros.h \
+  /Users/greg/eclipse/KataBankOCR/Externals/cpputest/include/CppUTest/TestResult.h \
+  /Users/greg/eclipse/KataBankOCR/Externals/cpputest/include/CppUTest/TestFailure.h \
+  /Users/greg/eclipse/KataBankOCR/Externals/cpputest/include/CppUTest/TestPlugin.h \
+  /Users/greg/eclipse/KataBankOCR/Externals/cpputest/include/CppUTest/MemoryLeakWarningPlugin.h \
+  /Users/greg/eclipse/KataBankOCR/Externals/cpputest/include/CppUTest/TestHarness_c.h \
+  /Users/greg/eclipse/KataBankOCR/Externals/cpputest/include/CppUTest/TestOutput.h \
+  /Users/greg/eclipse/KataBankOCR/Externals/cpputest/include/CppUTest/CommandLineArguments.h \
+  /Users/greg/eclipse/KataBankOCR/Externals/cpputest/include/CppUTest/TestFilter.h
+
+/Users/greg/eclipse/KataBankOCR/Externals/cpputest/include/CppUTest/CommandLineTestRunner.h:
+
+/Users/greg/eclipse/KataBankOCR/Externals/cpputest/include/CppUTest/TestHarness.h:
+
+/Users/greg/eclipse/KataBankOCR/Externals/cpputest/include/CppUTest/Utest.h:
+
+/Users/greg/eclipse/KataBankOCR/Externals/cpputest/include/CppUTest/SimpleString.h:
+
+/Users/greg/eclipse/KataBankOCR/Externals/cpputest/include/CppUTest/StandardCLibrary.h:
+
+/usr/include/c++/v1/string:
+
+/usr/include/c++/v1/__config:
+
+/usr/include/c++/v1/iosfwd:
+
+/usr/include/c++/v1/cstring:
+
+/usr/include/c++/v1/cstdio:
+
+/usr/include/c++/v1/cwchar:
+
+/usr/include/c++/v1/cwctype:
+
+/usr/include/c++/v1/cctype:
+
+/usr/include/c++/v1/algorithm:
+
+/usr/include/c++/v1/initializer_list:
+
+/usr/include/c++/v1/cstddef:
+
+/usr/include/c++/v1/type_traits:
+
+/usr/include/c++/v1/utility:
+
+/usr/include/c++/v1/__tuple:
+
+/usr/include/c++/v1/memory:
+
+/usr/include/c++/v1/typeinfo:
+
+/usr/include/c++/v1/exception:
+
+/usr/include/c++/v1/cstdint:
+
+/usr/include/c++/v1/new:
+
+/usr/include/c++/v1/limits:
+
+/usr/include/c++/v1/iterator:
+
+/usr/include/c++/v1/__functional_base:
+
+/usr/include/c++/v1/cstdlib:
+
+/usr/include/c++/v1/stdexcept:
+
+/Users/greg/eclipse/KataBankOCR/Externals/cpputest/include/CppUTest/UtestMacros.h:
+
+/Users/greg/eclipse/KataBankOCR/Externals/cpputest/include/CppUTest/TestResult.h:
+
+/Users/greg/eclipse/KataBankOCR/Externals/cpputest/include/CppUTest/TestFailure.h:
+
+/Users/greg/eclipse/KataBankOCR/Externals/cpputest/include/CppUTest/TestPlugin.h:
+
+/Users/greg/eclipse/KataBankOCR/Externals/cpputest/include/CppUTest/MemoryLeakWarningPlugin.h:
+
+/Users/greg/eclipse/KataBankOCR/Externals/cpputest/include/CppUTest/TestHarness_c.h:
+
+/Users/greg/eclipse/KataBankOCR/Externals/cpputest/include/CppUTest/TestOutput.h:
+
+/Users/greg/eclipse/KataBankOCR/Externals/cpputest/include/CppUTest/CommandLineArguments.h:
+
+/Users/greg/eclipse/KataBankOCR/Externals/cpputest/include/CppUTest/TestFilter.h:
